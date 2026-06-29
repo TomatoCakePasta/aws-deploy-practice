eaf58e3db0bbf75076d1720fdbe6ea5a296d1712
TomatoCakePasta <gaomuk50@gmail.com>
2026-06-29 21:27:25 +0900

    映画コンポーネントの実装

--- frontend/src/components/MovieCardComponent.vue
+++ frontend/src/components/MovieCardComponent.vue
@@ -3,8 +3,8 @@
         id: number
         title: string
         schedule: string
-        time: string
-        seat: string
+        endTime: string
+        seat: number
         imageUrl: string
     }
 
@@ -16,6 +16,90 @@
 
 <template>
     <div class="cards">
-        {{ movie.title }}
+        <div class="contents">
+            <div class="left-content">
+                <img :src="movie.imageUrl">
+            </div>
+            <div class="main-content">
+                <div class="text-area">
+                    <div>
+                        <p class="title">
+                            {{ movie.title }}
+                        </p>
+                        <p class="sub-info">
+                            {{ movie.schedule }} ~ {{ movie.endTime }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="right-content">
+                <div class="seat">
+                    <p v-if="movie.seat > 80">◎</p>
+                    <p v-else-if="movie.seat > 30">○</p>
+                    <p v-else-if="movie.seat > 15">△</p>
+                    <p v-else-if="movie.seat === 0">×</p>
+                </div>
+                <button class="btn" :disabled="movie.seat <= 0">
+                    予約
+                </button>
+            </div>
+        </div>
     </div>
 </template>
+
+<style scoped>
+.cards {
+    width: 1000px;
+    background-color: rgb(48, 135, 212);
+    color: white;
+    border-radius: 5px;
+}
+
+img {
+    width: 100px;
+    height: 100px;
+    margin: 10px;
+}
+
+.title {
+    font-size: xx-large;
+}
+
+.sub-info {
+    font-size:x-large
+}
+
+.main-content {
+    margin: 10px;
+}
+
+.text-area {
+    display: flex;
+}
+
+.seat {
+    width: 100px;
+    height: 100px;
+    display: flex;
+    margin-right: 20px;
+    font-size: 80px;
+    justify-content: center;
+    align-items: center;
+}
+
+.right-content {
+    display:flex;
+    margin: 10px;
+    margin-left: auto;
+}
+
+.contents {
+    display: flex;
+}
+
+.btn {
+    width: 100px;
+    height: 100px;
+    font-size: 25px;
+}
+</style>
--- frontend/src/views/SelectMovieView.vue
+++ frontend/src/views/SelectMovieView.vue
@@ -3,9 +3,12 @@
 import { ref } from "vue";
 
     const movies = ref([
-        {id: 1, title: "劇場版 HOGEHOGE", schedule: "MM:HH", time: "MM", seat: "available", imageUrl: "../assets/movie.png"},
-        {id: 2, title: "映画 HOGEHOGE", schedule: "MM:HH", time: "MM", seat: "available", imageUrl: "../assets/movie.png"},
-        {id: 2, title: "HOGE AND HOGE", schedule: "MM:HH", time: "MM", seat: "available", imageUrl: "../assets/movie.png"},
+        {id: 1, title: "劇場版 HOGEHOGE", schedule: "MM:HH", endTime: "HH:MM", seat: 100, imageUrl: "src/assets/movie.png"},
+        {id: 2, title: "映画 HOGEHOGE", schedule: "MM:HH", endTime: "HH:MM", seat: 50, imageUrl: "src/assets/movie.png"},
+        {id: 3, title: "HOGE AND HOGE", schedule: "MM:HH", endTime: "HH:MM", seat: 20, imageUrl: "src/assets/movie.png"},
+        {id: 4, title: "HOGE!! 2", schedule: "MM:HH", endTime: "HH:MM", seat: 67, imageUrl: "src/assets/movie.png"},
+        {id: 5, title: "HOGE!!! 3", schedule: "MM:HH", endTime: "HH:MM", seat: 0, imageUrl: "src/assets/movie.png"},
+        {id: 6, title: "HOGE the Movie", schedule: "MM:HH", endTime: "HH:MM", seat: 50, imageUrl: "src/assets/movie.png"},
     ]);
 </script>
 
