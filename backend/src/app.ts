567800849ee0504658fa79ed27ff32cf29fd49ae
TomatoCakePasta <gaomuk50@gmail.com>
2026-07-12 10:05:06 +0900

    実装: 映画リストを取得するGET API

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,39 @@
 import express, { type Express, type Request, type Response } from "express";
+import cors from "cors";
 
 const app: Express = express();
+const PORT = 3000;
+
+app.use(cors());
+
+interface Movie {
+    id: number;
+    title: string;
+    schedule: string;
+    endTime: string;
+    seat: number;
+    imageUrl: string;
+}
+
+const dummyMovies: Movie[] = [
+    { id: 1, title: "劇場版 HOGEHOGE", schedule: "10:20", endTime: "11:40", seat: 100, imageUrl: "src/assets/movie.png" },
+    { id: 2, title: "映画 HOGEHOGE", schedule: "09:05", endTime: "11:25", seat: 50, imageUrl: "src/assets/movie.png" },
+    { id: 3, title: "HOGE AND HOGE", schedule: "13:00", endTime: "15:20", seat: 20, imageUrl: "src/assets/movie.png" },
+    { id: 4, title: "HOGE!! 2", schedule: "11:30", endTime: "13:00", seat: 67, imageUrl: "src/assets/movie.png" },
+    { id: 5, title: "HOGE!!! 3", schedule: "14:15", endTime: "16:50", seat: 0, imageUrl: "src/assets/movie.png" },
+    { id: 6, title: "HOGE the Movie", schedule: "18:00", endTime: "20:20", seat: 50, imageUrl: "src/assets/movie.png" },
+];
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World");
 });
 
-app.listen(3000, () => {
-    console.log("Server started: http://localhost:3000");
+app.listen(PORT, () => {
+    console.log(`Server started: http://localhost:${PORT}`);
+});
+
+app.get("/movie-list", (req: Request, res: Response) => {
+    console.log("GET: movie-list");
+    res.json(dummyMovies);
+    console.log("response sent");
 });
--- frontend/src/views/SelectMovieView.vue
+++ frontend/src/views/SelectMovieView.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
     import MovieCardComponent from "@/components/MovieCardComponent.vue";
-    import { ref } from "vue";
+    import { onMounted, ref } from "vue";
+    import axios from "axios";
+    import type { Movie } from "@/stores/reservation";
 
-    const movies = ref([
-        {id: 1, title: "劇場版 HOGEHOGE", schedule: "10:20", endTime: "11:40", seat: 100, imageUrl: "src/assets/movie.png"},
-        {id: 2, title: "映画 HOGEHOGE", schedule: "09:05", endTime: "11:25", seat: 50, imageUrl: "src/assets/movie.png"},
-        {id: 3, title: "HOGE AND HOGE", schedule: "13:00", endTime: "15:20", seat: 20, imageUrl: "src/assets/movie.png"},
-        {id: 4, title: "HOGE!! 2", schedule: "11:30", endTime: "13:00", seat: 67, imageUrl: "src/assets/movie.png"},
-        {id: 5, title: "HOGE!!! 3", schedule: "14:15", endTime: "16:50", seat: 0, imageUrl: "src/assets/movie.png"},
-        {id: 6, title: "HOGE the Movie", schedule: "18:00", endTime: "20:20", seat: 50, imageUrl: "src/assets/movie.png"},
-    ]);
+    const movies = ref<Movie[]>([]);
+
+    onMounted(async () => {
+        try {
+            const res = await axios.get("http://localhost:3000/movie-list");
+            movies.value = res.data;
+            console.log(res.data);
+        }
+        catch(err) {
+            console.error("GET Failed: ", err);
+        }
+    });
 </script>
 
 <template>
@@ -19,4 +25,5 @@
         :key="movie.id"
         :movie="movie"
     />
+    <p v-if="movies">Loading ...</p>
 </template>
